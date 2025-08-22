5d2eca4f4e27a9aaf81a33b0301c746a6d614f03
root <root@localhost>
2025-08-22 22:10:09 +0700

    Test
    Test

---
+++ project/data/files/java/WidgetJob3.java
@@ -0,0 +1,56 @@
+package com.anbui.widget.battery;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Build;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import android.content.Intent;
+import android.app.PendingIntent;
+import android.util.Log;
+import android.appwidget.AppWidgetManager;
+
+public class WidgetJob3 extends JobService {
+    
+    private Context context;
+    private boolean isJobCancelled = false;
+    private Intent intent;
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        try {
+            if (!CheckBattery.isJobIdRunning(this, 456)) {
+                Intent intent = new Intent(this, BatteryWidget.class);
+                intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+                intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, CheckBattery.appWidgetId);
+                sendBroadcast(intent);
+                Log.i("WidgetJob3", "Start WidgetJob completed.");
+            } else {
+                Log.i("WidgetJob3", "WidgetJob is running.");
+            }
+        } catch (Exception _e) {
+            Log.e("WidgetJob3", "Failed!");
+            Log.e("WidgetJob3", _e.toString());
+        }
+        return true;
+    }
+    
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return true;
+    }
+    
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
